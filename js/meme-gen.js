21b42305aef7d5e43ef85cf40bade810bce3cd39
Super Sai-yan Monk <acharyasaiprasad@gmail.com>
2024-01-15 00:30:38 +0530

    load more fix

--- js/meme-gen.js
+++ js/meme-gen.js
@@ -1,23 +1,57 @@
 const memeBlocks = document.querySelectorAll(".individual-meme-block");
+const loadMoreBtn = document.querySelector(".load-more-btn-frame #load-more-btn");
+
 
 const updateDetails = (memeBlock, url, title, author) => {
     const memeImage = memeBlock.querySelector("img");
     const memeTitle = memeBlock.querySelector(".meme-title");
-    const memeAuthor = memeBlock.querySelector(".meme-author");
 
     memeImage.setAttribute("src", url);
     memeTitle.innerHTML = title;
-    memeAuthor.innerHTML = `Meme by: ${author}`;
 };
 
+const loadedMemeUrls = new Set();
+
 const generateMeme = (memeBlock) => {
-    fetch("https://meme-api.com/gimme/wholesomememes")
+console.log (memeBlock);
+    return fetch("https://meme-api.com/gimme/wholesomememes")
         .then((response) => response.json())
         .then((data) => {
-            updateDetails(memeBlock, data.url, data.title, data.author);
+            // Check if the meme URL has already been loaded
+            if (!loadedMemeUrls.has(data.url)) {
+                updateDetails(memeBlock, data.url, data.title, data.author);
+                loadedMemeUrls.add(data.url);
+            } else {
+                // If the meme URL is not unique, fetch again
+                return generateMeme(memeBlock);
+            }
         });
 };
 
+const loadMoreMemes = () => {
+    for (let i = 0; i < 10; i++) {
+        const newMemeBlock = document.createElement("div");
+        newMemeBlock.className = "individual-meme-block";
+        let memeGenerator = document.createElement("div");
+        memeGenerator.className = "meme-generator";
+        newMemeBlock.appendChild(memeGenerator);
+        let memeTitle = document.createElement("h2");
+        memeTitle.className = "meme-title";
+        memeTitle.innerText = "Loading...";
+        let memeImage = document.createElement("img");
+        memeImage.src = "";
+        memeImage.alt = "";
+        memeGenerator.appendChild(memeTitle);
+        memeGenerator.appendChild(memeImage);
+        document.querySelector(".main-meme-block").appendChild(newMemeBlock);
+
+        generateMeme(newMemeBlock);
+    }
+};
+
+loadMoreBtn.addEventListener("click", loadMoreMemes);
+
+// Initial load of memes
 memeBlocks.forEach((block) => {
     generateMeme(block);
 });
